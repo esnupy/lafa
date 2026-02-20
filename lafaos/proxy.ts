c5f698fed7d3686ae11b2113eaa293fd5f4a52ed
Esnupy <esnupy333@gmail.com>
2026-02-20 09:50:07 -0600

    refactor: migrar de middleware a proxy (Next.js 16)

--- lafaos/proxy.ts
+++ lafaos/proxy.ts
@@ -1,7 +1,7 @@
 import { type NextRequest } from "next/server";
 import { updateSession } from "./lib/supabase/middleware";
 
-export const middleware = async (request: NextRequest) =>
+export const proxy = async (request: NextRequest) =>
   updateSession(request);
 
 export const config = {
